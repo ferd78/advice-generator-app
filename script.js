3e8b499d1e5fe93b564a537319c60c46558becaa
ferd78 <114067344+ferd78@users.noreply.github.com>
2024-02-06 22:25:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const fetchAdvice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    console.log(data)
+
+    document.getElementById("advice-number").innerHTML = `Advice #${data.slip.id}`
+    document.getElementById("quote").innerHTML = `"${data.slip.advice}"`    
+}
+
+fetchAdvice()
